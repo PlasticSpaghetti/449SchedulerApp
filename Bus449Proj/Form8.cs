5f03a7afc37469e5e13f150f8d548974bd5c225f
joshg <jagraige@ncsu.edu>
2018-04-15 19:08:30 -0400

    Added dropshadow

--- Bus449Proj/Form8.cs
+++ Bus449Proj/Form8.cs
@@ -12,7 +12,16 @@ namespace Bus449Proj
 {
     public partial class Form8 : Form
     {
-
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
 
 
 
